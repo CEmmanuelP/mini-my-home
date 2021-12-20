8ac4bd1fff43f31466a6db1668799075bea24b5d
CEmmanuelP <chrisp3@daum.net>
2021-12-20 17:02:53 +0900

    first commit

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <div
+                style={{ display: "inline-block", color: "var(--color-green)" }}
+            >
+                Header
+            </div>
+        </HeaderContainer>
+    );
+};
+
+const HeaderContainer = styled.div`
+    border: 1px solid var(--color-primary);
+    width: 100%;
+    max-width: 1050px;
+    height: 4rem;
+    text-align: center;
+`;
+
+export default Header;
---
+++ src/Layout.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+    return (
+        <Layouts>
+            <Header />
+            <>{children}</>
+            <Footer />
+        </Layouts>
+    );
+};
+
+const Layouts = styled.div`
+    width: 100%;
+    min-width: 1050px;
+    height: 100vh;
+    background-image: url("/img/background_msrh.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
+
+export default Layout;
