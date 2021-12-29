d3f99bc21800bf05dc3225029743f1e0e4ba6184
CEmmanuelP <chrisp3@daum.net>
2021-12-29 15:33:40 +0900

    detail info redux

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,45 +1,53 @@
 const initialState = {
-  RESULT: [],
-  list_total_count: "",
-  row: [],
+    RESULT: [],
+    list_total_count: "",
+    row: [],
 };
 
 export const getDataAction = (data) => {
-  console.log(data);
-  return {
-    type: "ADD_DATA",
-    data: data,
-  };
+    console.log(data);
+    return {
+        type: "ADD_DATA",
+        data: data,
+    };
 };
 
 export const getDetailDataAction = (id) => {
-  return {
-    id: id,
-  };
+    return {
+        type: "GET_DETAIL_DATA",
+        id: id,
+    };
 };
 
 const dataReducer = (state = initialState, action) => {
-  console.log("dataReducer");
-  console.log(action);
-  console.log(state);
-  switch (action.type) {
-    case "ADD_DATA": {
-      return {
-        ...state,
-        RESULT: action.data.RESULT,
-        list_total_count: action.data.list_total_count,
-        row: action.data.row,
-      };
+    console.log("dataReducer");
+    console.log(action);
+    console.log(state);
+    switch (action.type) {
+        case "ADD_DATA": {
+            return {
+                ...state,
+                RESULT: action.data.RESULT,
+                list_total_count: action.data.list_total_count,
+                row: action.data.row,
+            };
+        }
+        case "GET_DETAIL_DATA": {
+            const detail = state.row.filter(
+                (element) => element.SVCID === `${action.id.id}`
+            );
+
+            return {
+                ...state,
+                detail: detail[0],
+            };
+        }
+        default: {
+            return {
+                ...state,
+            };
+        }
     }
-    case "GET_DETAIL_DATA": {
-      return state.row.filter((element) => element.SVCID === action.id);
-    }
-    default: {
-      return {
-        ...state,
-      };
-    }
-  }
 };
 
 export default dataReducer;
