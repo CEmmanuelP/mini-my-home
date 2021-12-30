06214c2645af0b41bbc0461f5e1dda3cefbca588
CEmmanuelP <chrisp3@daum.net>
2021-12-30 16:19:56 +0900

    kakao login 1

--- src/config.js
+++ src/config.js
@@ -4,3 +4,5 @@ export const CORS_URL = "https://secret-ocean-49799.herokuapp.com";
 //http://openAPI.seoul.go.kr:8088/485051717663687238356663674753/xml/ListPublicReservationSport/1/5/농구장
 
 export const SERVICE_URL = "http://openAPI.seoul.go.kr:8088";
+
+export const KAKAO_APP_KEY = "a21150e71cee4924455798504e08b6c1";
